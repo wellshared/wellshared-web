6eaedadb05a3da774d0f083e35a2d64d703fb6ee
Gerard <gorteganel@gmail.com>
2019-10-17 19:13:31 +0200

    build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  url: 'http://de.wellshared.es/slim-wellshared/public_html'
+  url: 'https://wellshrd.herokuapp.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  url: 'http://localhost:8080'
+  url: 'https://wellshrd.herokuapp.com'
 };

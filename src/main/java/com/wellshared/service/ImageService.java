20e5e0f402b46aa12b992fe6a2a3be04ed8e13df
Gerard <gortega@sctrade.es>
2019-10-28 17:14:57 +0100

    aws buckets

--- src/main/java/com/wellshared/service/ImageService.java
+++ src/main/java/com/wellshared/service/ImageService.java
@@ -1,9 +1,22 @@
 package com.wellshared.service;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.wellshared.model.Center;
 import com.wellshared.model.Image;
 import com.wellshared.repository.ImageRepository;
@@ -12,15 +25,48 @@ import com.wellshared.repository.ImageRepository;
 public class ImageService {
 	@Autowired
 	private ImageRepository imageRepository;
-	
-	public Image saveImage(MultipartFile file, Center center) throws Exception{
+
+	public Image saveImage(MultipartFile file, Center center) throws Exception {
 		Image image;
 		try {
-			image = new Image(center, file.getOriginalFilename(), file.getBytes());
-			imageRepository.save(image);
-		} catch(Exception e) {
+			image = new Image(center, file.getOriginalFilename(), null);
+			// imageRepository.save(image);
+			this.uploadImage(file);
+		} catch (Exception e) {
 			throw new Exception("No se ha encontrado la ruta del fichero");
 		}
 		return image;
+	}
+
+	public void uploadImage(MultipartFile file) {
+		try {
+			AWSCredentials credentials = new BasicAWSCredentials("wellshared", "wellshared");
+			AmazonS3 s3client = AmazonS3ClientBuilder.standard()
+					.withCredentials(new AWSStaticCredentialsProvider(credentials)).withRegion(Regions.EU_WEST_3)
+					.build();
+			String bucketName = "wellshrd-assets";
+
+			// Upload a file as a new object with ContentType and title specified.
+			PutObjectRequest request = new PutObjectRequest(bucketName, "vobpr8zqxnhy/" + file.getName(),
+					this.convert(file));
+			s3client.putObject(request);
+		} catch (AmazonServiceException e) {
+			// The call was transmitted successfully, but Amazon S3 couldn't process
+			// it, so it returned an error response.
+			e.printStackTrace();
+		} catch (Exception e) {
+			// Amazon S3 couldn't be contacted for a response, or the client
+			// couldn't parse the response from Amazon S3.
+			e.printStackTrace();
+		}
+	}
+
+	public File convert(MultipartFile file) throws IOException {
+		File convFile = new File(file.getOriginalFilename());
+		convFile.createNewFile();
+		try (InputStream is = file.getInputStream()) {
+			Files.copy(is, convFile.toPath());
+		}
+		return convFile;
 	}
 }

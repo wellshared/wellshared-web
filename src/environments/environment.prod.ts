e10cc6a8a25d40714f6fbf727b3facaea4552055
Gerard <gorteganel@gmail.com>
2019-12-16 20:02:05 +0100

    chaptcha v2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   url: 'https://wellshrd-test.herokuapp.com',
-  recaptcha: '6LddCccUAAAAAOAFpRin8-6RjHi5WAHqC97eUdsl'
+  recaptcha: '6LczCcgUAAAAAL7sKqrvDApQKk_-W2ZhNvoRB6px'
 };

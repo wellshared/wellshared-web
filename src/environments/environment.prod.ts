f8720079a4904d62456217971b49e8d83c0bf982
Gerard <gorteganel@gmail.com>
2019-12-16 19:31:11 +0100

    test

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  url: 'https://wellshrd.herokuapp.com',
+  url: 'https://wellshrd-test.herokuapp.com',
   recaptcha: '6LddCccUAAAAAOAFpRin8-6RjHi5WAHqC97eUdsl'
 };
